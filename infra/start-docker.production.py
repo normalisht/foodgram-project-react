74806e948dbbf548ba3d7952fbc0615ba1557f42
Losharik17 <dimanormanev@yandex.ru>
2023-07-05 22:28:47 +0300

    Prod

--- infra/start-docker.production.py
+++ infra/start-docker.production.py
@@ -7,10 +7,11 @@ from dotenv import load_dotenv
 SUCCESS_LAUNCH = 'All processes up and running\r\n'
 DOCKER_COMPOSE_COMMAND = ['sudo', 'docker', 'compose', '-f',
                           'docker-compose.production.yml']
+print(*DOCKER_COMPOSE_COMMAND)
 
 subprocess.run(['pip', 'install', 'docker-compose-wait'])
 subprocess.run([*DOCKER_COMPOSE_COMMAND, 'down'])
-subprocess.run([DOCKER_COMPOSE_COMMAND, 'up', '-d'])
+subprocess.run([*DOCKER_COMPOSE_COMMAND, 'up', '-d'])
 
 result = subprocess.run(
     ['docker-compose-wait', '-f', 'docker-compose.production.yml'],
@@ -24,16 +25,16 @@ while result.stdout.decode('UTF-8') != SUCCESS_LAUNCH:
         stdout=subprocess.PIPE
     )
 
-subprocess.run(['docker-compose', 'exec', 'backend', 'python',
+subprocess.run([*DOCKER_COMPOSE_COMMAND, 'exec', 'backend', 'python',
                 'manage.py', 'migrate'])
 
-subprocess.run(['docker-compose', 'exec', 'backend', 'python',
+subprocess.run([*DOCKER_COMPOSE_COMMAND, 'exec', 'backend', 'python',
                 'manage.py', 'load_data'])
 
-subprocess.run(['docker-compose', 'exec', 'backend', 'python',
+subprocess.run([*DOCKER_COMPOSE_COMMAND, 'exec', 'backend', 'python',
                 'manage.py', 'collectstatic', '--noinput'])
 
-subprocess.run(['docker-compose', 'exec', 'backend', 'cp',
+subprocess.run([*DOCKER_COMPOSE_COMMAND, 'exec', 'backend', 'cp',
                 '-r', '/app/collected_static/.', '/backend_static/static/'])
 
 load_dotenv()
@@ -42,7 +43,7 @@ ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
 ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
 ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
 
-subprocess.run(['docker-compose', 'exec', 'backend', 'python',
+subprocess.run([*DOCKER_COMPOSE_COMMAND, 'exec', 'backend', 'python',
                 'manage.py', 'shell', '-c',
                 ("from users.models import User; "
                  f"User.objects.create_superuser('{ADMIN_USERNAME}', "
